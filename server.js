93473e58318be3bca80657855e4abb093645a581
Gilad Tsabar <gilad@tsabar.net>
2020-09-20 13:44:41 +0200

    heroku ready

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 const port = process.env.PORT || 5000;
 //Static file declaration
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, '/client/build')));
 //production mode
 if(process.env.NODE_ENV === 'production') {  
-  app.use(express.static(path.join(__dirname, 'client/build')));  
+  app.use(express.static(path.join(__dirname, '/client/build')));  
   app.get('*', (req, res) => {    
-    res.sendFile(path.join(__dirname = 'client/build/index.html'));
+    res.sendFile(path.join(__dirname = '/client/build/index.html'));
   });
 }
 //build mode
